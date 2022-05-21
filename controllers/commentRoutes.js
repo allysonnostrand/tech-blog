7aceb3f4b39f698cdd55a6b3168afdf0f7deb7d4
iggy <allyson.nostrandd@gmail.com>
2022-05-21 16:17:05 -0700

    created comment routes and tested in insomnia

---
+++ controllers/commentRoutes.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+const {User,Post,Comment} = require("../models/");
+
+//find all comments
+router.get("/", (req, res) => {
+  Comment.findAll({
+    include:[User, Post]
+  })
+    .then(allComments => {
+      res.json(allComments);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ msg: "an error occured", err });
+    });
+});
+
+//find one comment
+router.get("/:id", (req, res) => {
+  Comment.findByPk(req.params.id,{
+      include: [User, Post]
+  })
+    .then(oneComment => {
+      res.json(oneComment);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ msg: "an error occured", err });
+    });
+});
+
+//create new comment
+router.post("/", (req, res) => {
+ Comment.create(req.body)
+    .then(newComment => {
+      req.body.comment = {
+        title:newComment.title,
+        text:newComment.text
+      }
+      res.json(newComment);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ msg: "an error occured", err });
+    });
+});
+
+//update comment
+router.put("/:id",(req,res)=>{
+    Comment.update(req.body,{
+        where: {
+            id: req.params.id
+        }
+    }).then (upComment => {
+        res.json(upComment);
+    }).catch (err => {
+        console.log(err);
+        res.status(500).json({ msg: "an error occured", err});
+    })
+  })
+  
+
+//delete comment
+router.delete("/:id",(req,res)=>{
+  Comment.destroy({
+      where: {
+          id: req.params.id
+      }
+  }).then (delComment => {
+      res.json(delComment);
+  }).catch (err => {
+      console.log(err);
+      res.status(500).json({ msg: "an error occured", err});
+  })
+})
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -7,4 +7,7 @@ router.use("/api/users",userRoutes)
 const postRoutes = require("./postRoutes");
 router.use("/api/posts",postRoutes)
 
+const commentRoutes = require("./commentRoutes");
+router.use("/api/comments",commentRoutes)
+
 module.exports = router;
