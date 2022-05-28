85a4197785ee8991121e1a827dd3cae79ec8888b
iggy <allyson.nostrandd@gmail.com>
2022-05-27 19:34:52 -0700

    still trying to fix heroku deploy

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const session = require('express-session');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const { User, Post, Comment } = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const { User, Post, Comment } = require('./models');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const sess = {
   secret: process.env.SESSION_SECRET,
@@ -25,14 +27,12 @@ const sess = {
 
 app.use(session(sess));
 
+app.use(express.static('public'));
+
 const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
 
 app.use("/", routes);
 
